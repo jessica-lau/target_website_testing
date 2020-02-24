711e6e0a2162f22d16dda35dcf0be8ef4118e170
jessica-lau <jessica.lau2@gmail.com>
2020-02-23 23:00:26 -0500

    added filter store by services script

---
+++ __tests__/target_favorites.spec.js
@@ -0,0 +1,65 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By;
+require('selenium-webdriver/testing');
+require('dotenv').config();
+var assert = require('assert');
+let chrome = require('selenium-webdriver/chrome');
+
+describe("Store Location", () => {
+    let driver;
+
+    before(async () => {
+        var capabilities = {
+            'browserName': 'Chrome',
+            'browser_version': '60.0',
+            'os': 'Windows',
+            'os_version': '10',
+            'resolution': '1920x1080',
+            'browserstack.user': process.env.browserstack_user,
+            'browserstack.key': process.env.browserstack_key,
+            'name': 'B Sample Test'
+        };
+        //Note: run test on browserstacker:
+
+        // var builder = new webdriver.Builder().
+        //   usingServer('http://hub-cloud.browserstack.com/wd/hub').
+        //   withCapabilities(capabilities);
+        // driver = await builder.build();
+
+        //Note: run test on local broswer:
+        let localChromeOption = new chrome.Options();
+        localChromeOption.addArguments("--start-maximized");
+        localChromeOption.addArguments("--disable-web-security");
+        localChromeOption.addArguments("--allow-running-insecure-content");
+        localChromeOption.setAcceptInsecureCerts(true)
+        driver = await new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .setChromeOptions(localChromeOption)
+            .build();
+
+        console.log("started testing");
+        // driver.manage().window().maximize();
+        await driver.get('http://www.target.com')
+
+    });
+
+    after(async () => {
+    await driver.quit();
+    });
+
+    describe("Header", () => {
+        it("should exist", async () => {
+            await driver.sleep(5000);
+            let title = await driver.getTitle()
+            console.log(assert.equal(title, "Target : Expect More. Pay Less."));
+        });
+
+        it("change store location", async () => {
+            let favorites = await driver.findElement(By.css(`[data-test="crushHeader"]`));
+            let isFavoritesDisplayed = await favorites.isDisplayed();
+            assert.equal(isFavoritesDisplayed, 1);
+            await favorites.click();
+            await driver.sleep(5000);
+        });
+    });
+});
---
+++ __tests__/target_find_stores.spec.js
@@ -0,0 +1,80 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By;
+require('selenium-webdriver/testing');
+require('dotenv').config();
+var assert = require('assert');
+let chrome = require('selenium-webdriver/chrome');
+
+describe("Store Location", () => {
+    let driver;
+
+    before(async () => {
+        var capabilities = {
+            'browserName': 'Chrome',
+            'browser_version': '60.0',
+            'os': 'Windows',
+            'os_version': '10',
+            'resolution': '1920x1080',
+            'browserstack.user': process.env.browserstack_user,
+            'browserstack.key': process.env.browserstack_key,
+            'name': 'B Sample Test'
+        };
+        //Note: run test on browserstacker:
+
+        // var builder = new webdriver.Builder().
+        //   usingServer('http://hub-cloud.browserstack.com/wd/hub').
+        //   withCapabilities(capabilities);
+        // driver = await builder.build();
+
+        //Note: run test on local broswer:
+        let localChromeOption = new chrome.Options();
+        localChromeOption.addArguments("--start-maximized");
+        localChromeOption.addArguments("--disable-web-security");
+        localChromeOption.addArguments("--allow-running-insecure-content");
+        localChromeOption.setAcceptInsecureCerts(true)
+        driver = await new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .setChromeOptions(localChromeOption)
+            .build();
+
+        console.log("started testing");
+        // driver.manage().window().maximize();
+        await driver.get('http://www.target.com')
+
+    });
+
+    after(async () => {
+    await driver.quit();
+    });
+
+    describe("Header", () => {
+        it("should exist", async () => {
+            await driver.sleep(5000);
+            let title = await driver.getTitle()
+            console.log(assert.equal(title, "Target : Expect More. Pay Less."));
+        });
+
+        it("change store location", async () => {
+            let findStore = await driver.findElement(By.linkText(`Find Stores`));
+            let isFindStoreDisplayed = await findStore.isDisplayed();
+            assert.equal(isFindStoreDisplayed, 1);
+            await findStore.click();
+            await driver.sleep(5000);
+            let filterServices = await driver.findElement(By.css(`[data-test="storeLocator-filterByServicesButton"]`));
+            let isFilterServicesDisplayed = await filterServices.isDisplayed();
+            assert.equal(isFilterServicesDisplayed, 1);
+            await filterServices.click();
+            await driver.sleep(5000);
+            let checkboxServices = await driver.findElement(By.css(`[for="Opt"]`));
+            let isCheckboxServicesDisplayed = await checkboxServices.isDisplayed();
+            assert.equal(isCheckboxServicesDisplayed, 1);
+            await checkboxServices.click();
+            await driver.sleep(10000);
+            let doneFiltering = await driver.findElement(By.css(`[data-test="storeLocator-filterByServicesDoneButton"]`));
+            let isDoneFilteringDisplayed = await doneFiltering.isDisplayed();
+            assert.equal(isDoneFilteringDisplayed, 1);
+            await filterServices.click();
+            await driver.sleep(5000);
+        });
+    });
+});
