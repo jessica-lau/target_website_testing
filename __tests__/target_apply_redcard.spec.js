cf864ad361b95b1cc0ca6d6ee6814fcec87952f4
jessica-lau <jessica.lau2@gmail.com>
2020-04-14 13:22:29 -0400

    create test scripts for security form

--- __tests__/target_apply_redcard.spec.js
+++ __tests__/target_apply_redcard.spec.js
@@ -85,16 +85,19 @@ describe("Store Location", () => {
             console.log(currentWindow);
             await driver.switchTo().window(windows[1]);
 
+            //Personal Information Form
             let firstName = driver.findElement(By.id(`txtFirstName`));
             let isFirstNameDisplayed = await firstName.isDisplayed();
             assert.equal(isFirstNameDisplayed, 1);
             await firstName.click();
             await firstName.sendKeys('Jessica');
+
             let lastName = driver.findElement(By.id(`txtLastName`));
             let isLastNameDisplayed = await lastName.isDisplayed();
             assert.equal(isLastNameDisplayed, 1);
             await lastName.click();
             await lastName.sendKeys('None');
+
             let phoneNumber = driver.findElement(By.id(`txtPhonePrimary`));
             let isPhoneNumberDisplayed = await phoneNumber.isDisplayed();
             assert.equal(isPhoneNumberDisplayed, 1);
@@ -115,12 +118,12 @@ describe("Store Location", () => {
             assert.equal(isEmailDisplayed, 1);
             await email.click();
             await email.sendKeys('freelancegigtest@gmail.com');
+
             let confirmEmail = driver.findElement(By.id(`txtConfirmEmail`));
             let isConfirmEmailDisplayed = await confirmEmail.isDisplayed();
             assert.equal(isConfirmEmailDisplayed, 1);
             await confirmEmail.click();
             await confirmEmail.sendKeys('freelancegigtest@gmail.com');
-            await driver.sleep(5000);
 
             let continueButton = driver.findElement(By.css(`[value="continue  "]`));
             let isContinueButtonDisplayed = await continueButton.isDisplayed();
@@ -128,6 +131,75 @@ describe("Store Location", () => {
             await continueButton.click();
             await driver.sleep(3000);
 
+            //Security Information Form
+
+            //***Alternative code to select birth month in dropdown
+            // let birthMonth = driver.findElement(By.id(`birthdateMonth`));
+            // let isBirthMonthDisplayed = await birthMonth.isDisplayed();
+            // assert.equal(isBirthMonthDisplayed, 1);
+            // await birthMonth.click();
+            // let selectMonth = await birthMonth.findElements(By.css(`option`));
+            // let isSelectMonthDisplayed = await selectMonth[2].isDisplayed();
+            // assert.equal(isSelectMonthDisplayed, 1);
+            // await selectMonth[2].click();
+            // await driver.sleep(3000);
+
+            let birthMonth = driver.findElement(By.id(`birthdateMonth`));
+            let isBirthMonthDisplayed = await birthMonth.isDisplayed();
+            assert.equal(isBirthMonthDisplayed, 1);
+            await birthMonth.click();
+            let selectMonth = driver.findElement(By.css(`[value="2"]`));
+            let isSelectMonthDisplayed = await selectMonth.isDisplayed();
+            assert.equal(isSelectMonthDisplayed, 1);
+            await selectMonth.click();
+
+            let birthDay = driver.findElement(By.id(`birthdateDay`));
+            let isBirthDayDisplayed = await birthDay.isDisplayed();
+            assert.equal(isBirthDayDisplayed, 1);
+            await birthDay.click();
+            let selectDay = driver.findElement(By.css(`[value="15"]`));
+            let isSelectDayDisplayed = await selectDay.isDisplayed();
+            assert.equal(isSelectDayDisplayed, 1);
+            await selectDay.click();
+
+            let birthYear = driver.findElement(By.id(`birthdateYear`));
+            let isBirthYearDisplayed = await birthYear.isDisplayed();
+            assert.equal(isBirthYearDisplayed, 1);
+            await birthYear.click();
+            await birthYear.sendKeys('1995');
+
+            let socialNumber = driver.findElement(By.id(`txtSSN1`));
+            let isSocialNumberDisplayed = await socialNumber.isDisplayed();
+            assert.equal(isSocialNumberDisplayed, 1);
+            await socialNumber.click();
+            await socialNumber.sendKeys('1111111111');
+
+            let socialNumberConfirm = driver.findElement(By.id(`txtSSNConfirm1`));
+            let isSocialNumberConfirmDisplayed = await socialNumberConfirm.isDisplayed();
+            assert.equal(isSocialNumberConfirmDisplayed, 1);
+            await socialNumberConfirm.click();
+            await socialNumberConfirm.sendKeys('1111111111');
+
+            let idNumber = driver.findElement(By.id(`idNum`));
+            let isIdNumberDisplayed = await idNumber.isDisplayed();
+            assert.equal(isIdNumberDisplayed, 1);
+            await idNumber.click();
+            await idNumber.sendKeys('SDK8888888');
+
+            let idState = driver.findElement(By.id(`idState`));
+            let isIdStateDisplayed = await idState.isDisplayed();
+            assert.equal(isIdStateDisplayed, 1);
+            await idState.click();
+            let selectState = driver.findElement(By.css(`[value= "MA"]`));
+            let isSelectStateDisplayed = await selectState.isDisplayed();
+            assert.equal(isSelectStateDisplayed, 1);
+            await selectState.click();
+
+            let continueButton2 = driver.findElement(By.css(`[value= "continue  "]`));
+            let isContinueButton2Displayed = await continueButton2.isDisplayed();
+            assert.equal(isContinueButton2Displayed, 1);
+            await continueButton2.click();
+            await driver.sleep(5000);
         });
 
     });
